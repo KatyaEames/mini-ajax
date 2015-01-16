f0b7f4a987927968b49f7a2ecf84e34a8ade0371
KatyaEames <katya.eames@gmail.com>
2015-01-16 12:37:26 -0700

    doc ready

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-$('#getUsers').on('click', function() {
-	return $.ajax({
-		method: 'GET',
-		url: 'http://reqr.res/api/users?page=1',
-		success: function(res) {
-			console.log(res);
-		}
-	})
-})
-var insertData = function(arr) {
-	for (var i = 0; i < arr.lrngth; i++) {
-		$('#userInfo' + (i + 1)).html('div + 
-			User Info:' +
-			'<li>' +
-			'First name:' +
-			'</li>' +
-			'<li>' +
-			'Last name:' +
-			'</li>' +
-			'<hr>' +
-			'</div>'
-		)
+$(document).ready(function() {
+	$('#getUsers').on('click', function() {
+		return $.ajax({
+			method: 'GET',
+			url: 'http://reqr.res/api/users?page=1',
+			success: function(res) {
+				console.log(res);
+			}
+		});
+	});
+
+	var insertData = function(arr) {
+		for (var i = 0; i < arr.lrngth; i++) {
+			$('#userInfo' + (i + 1)).html('div + 
+				User Info:' +
+				'<li>' +
+				'First name:' +
+				'</li>' +
+				'<li>' +
+				'Last name:' +
+				'</li>' +
+				'<hr>' +
+				'</div>'
+			)
+		};
 	};
-}
-$('#getUsers').on('click', function() {
-	return $.ajax({
-		method: 'GET',
-		url: 'http://reqr.es/api/users?page=1',
-		success: function(res) {
-			console.log(res);
-			insertData(res.data);
-		}
-	})
-}
+
+	$('#getUsers').on('click', function() {
+		return $.ajax({
+			method: 'GET',
+			url: 'http://reqr.es/api/users?page=1',
+			success: function(res) {
+				console.log(res);
+				insertData(res.data);
+			}
+		})
+	}
+	
+});
